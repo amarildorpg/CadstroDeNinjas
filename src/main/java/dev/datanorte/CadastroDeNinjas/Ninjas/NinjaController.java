8b51aca14b3bd337ecc31aaf32152a7890825340
amarildorpg <amarildorpg@gmail.com>
2026-03-15 21:35:52 -0400

    feat: Atualiza o controller com mock de dados

--- src/main/java/dev/datanorte/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/datanorte/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.datanorte.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,4 +9,36 @@ public class NinjaController {
     public String boasVindas() {
         return "Boas vindas";
     }
+    //Adicionar ninja (CREATE)
+
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja Criado";
+    }
+
+    //Mostrar todos os ninjas (READ)
+
+    @GetMapping("/todos")
+    public String todosOsNinjas() {
+        return "Todos os Ninjas";
+    }
+
+    //Procurar Ninja por ID (READ)
+
+    @GetMapping("/todosid")
+    public String todosOsNinjasPorId() {
+        return "O ninja escolhido por ID";
+    }
+    //Alterar dados (UPDATE)
+    @PutMapping("/alterarid")
+    public String alterarNinjaPorId() {
+        return "O ninja escolhido por ID foi alterado";
+    }
+
+    //Deletar ninja (DELETE)
+    @DeleteMapping("/deletarid")
+    public String deletarNinjaPorId() {
+        return "O ninja escolhido por ID foi deletado";
+    }
+
 }
