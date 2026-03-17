ea950e2064b497b61c2bcf12cd9d4f28aa22f6b8
amarildorpg <amarildorpg@gmail.com>
2026-03-16 22:37:43 -0400

    chore: Incluir a versão do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos

--- src/main/java/dev/datanorte/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/datanorte/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,19 @@
 package dev.datanorte.CadastroDeNinjas.Ninjas;
 
+import dev.datanorte.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private int idade;
+    private String rank;
+    private MissoesModel missoes;
 }
